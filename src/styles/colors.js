c4bfdcf7e0fd0f85790b34098047fcec214920be
Daren Malfait <malfada@cronos.be>
2019-09-27 13:49:42 +0200

    add new utility classes

---
+++ src/styles/colors.js
@@ -0,0 +1,25 @@
+/* eslint-disable sort-keys */
+// const raccoonsBlue = '#000FFF';
+const white = '#fff';
+
+const wheelhouseGreen = '#56c186';
+const wheelhouseBlue = '#32435e';
+const wheelhouseDarkBlue = '#1c2738';
+
+const materialShadowNear =  '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)';
+const materialShadowMid =  '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)';
+const materialShadowFar =  '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)';
+const materialShadowTransition = 'all 0.3s cubic-bezier(.25,.8,.25,1)';
+
+export default {
+  primary: wheelhouseGreen,
+  secundary: wheelhouseBlue,
+  background: wheelhouseDarkBlue,
+  textDark: wheelhouseDarkBlue,
+  textLight: white,
+  // shadows
+  materialShadowNear,
+  materialShadowMid,
+  materialShadowFar,
+  materialShadowTransition,
+};

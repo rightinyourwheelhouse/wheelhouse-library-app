d9ac446fb3bdfee08be028f6dfa9be71216b08e3
Rafael <rafael@wheelhouse.be>
2019-10-31 15:29:50 +0100

    Login tryout

--- src/components/Views/Login/Login.js
+++ src/components/Views/Login/Login.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import qs from 'query-string';
+import * as userActionFile from '../../../redux/actions/users';
 
-export default ({ location }) => {
+const mapDispatchToProps = dispatch => ({
+  userActions: bindActionCreators(userActionFile, dispatch),
+});
+
+const mapStateToProps = state => ({
+  ...state,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(({
+  location,
+  userActions,
+}) => {
   // We extract the code we get from Slack if available
   // This code can be used as an access token by the API
   // Therefore we need to send it off to the API before the user can interact with the app
   const { code } = qs.parse(location.search);
+  let result = (
+    <p>Loading...</p>
+  );
 
   // Redirect to Slack login when there's no code and not authenticated
   if (!code) {
-    window.location.href = 'https://slack.com/oauth/authorize?response_type=code&redirect_uri=&scope=identity.basic identity.email identity.avatar identity.team&client_id=20329357267.717373150117';
+    result = <a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.avatar&client_id=20329357267.759347069140"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>;
   } else {
-    authorize(code).then(console.log);
-    // return (<Redirect to="/" />);
+    userActions.login(code);
   }
 
   // The actual login is managed by Slack. We effectively show nothing. Pure functional component
-  return null;
-};
+  return result;
+});
--- src/redux/actions/users/index.js
+++ src/redux/actions/users/index.js
@@ -6,6 +6,22 @@ const fetchAllUsersFailed = error => ({ type: actionTypes.FETCH_ALL_USERS_FAILED
 
 export const setActiveUser = user => ({ type: actionTypes.SET_ACTIVE_USER, user });
 
+export function login(code) {
+  return () => {
+    fetch(`${baseURL}${namespace}/auth/slack?code=${code}`).then((response) => {
+      if (response.ok) {
+        return response;
+      }
+      throw new Error('Could not log in properly');
+    }, (error) => {
+      throw error;
+    }).then(response => response.json())
+      .then((response) => {
+        console.log('%cLOGIN RESPONSE', 'background-color: #5a9ac8; padding: 5px; border-radius: 3px; font-weight: bold; color: white', response);
+      });
+  };
+}
+
 export function fetchAllUsers() {
   return (dispatch) => {
     fetch(`${baseURL}${namespace}/users`).then((response) => {

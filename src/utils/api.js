db8cf59919f38bda638a105e6f24a45992cc5071
Rafael <rafael@wheelhouse.be>
2019-11-08 16:40:10 +0100

    Authentication header util and setter

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,3 +2,20 @@
 // We'd have a staging and production environment depending on where we deploy
 export const baseURL = 'http://localhost:3000';
 export const namespace = '/api/v1';
+
+export function getApiKeyFromActiveUser() {
+  if (localStorage.getItem('active-user')) {
+    return JSON.parse(localStorage.getItem('active-user'))['api-key'];
+  }
+  return null;
+}
+
+export const authenticationHeader = {
+  headers: {
+    Authorization: `Bearer ${getApiKeyFromActiveUser()}`,
+  },
+};
+
+export function updateAuthenticationHeader(token) {
+  authenticationHeader.headers = { Authorization: `Bearer ${token}` };
+}

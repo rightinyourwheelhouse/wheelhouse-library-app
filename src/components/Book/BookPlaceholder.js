d03ddb94eb063cca6bde917e38e7f140092cf661
Rafael Mindreau <rafael.mindreau@gmail.com>
2020-02-11 16:04:26 +0100

    Fix WLA-49

--- src/components/Book/BookPlaceholder.js
+++ src/components/Book/BookPlaceholder.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default ({ Action }) => (
   <div className="book-placeholder-container">
     <div onClick={Action} role="button" className="book">
-      <FontAwesomeIcon icon={faPlus} />
+      <FontAwesomeIcon icon={faPlus} className="icon" />
       <div className="info">
         <h2 className="book-title">Add your own book to the collection</h2>
         <div className="rent-status">
